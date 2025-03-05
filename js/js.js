355349c9437ae64115b1a67762f09cfedd3eb3d2
bigboy595 <mishanka2008@gmail.com>
2025-03-05 16:05:39 +0300

    demi

--- js/js.js
+++ js/js.js
@@ -1,1 +1,120 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+
 console.log('hey niga')
+  let thumb = slider.querySelector('.thumb');
+  
+
+    thumb.onmousedown = function(event) {
+      event.preventDefault(); // предотвратить запуск выделения (действие браузера)
+
+      let shiftX = event.clientX - thumb.getBoundingClientRect().left;
+      // shiftY здесь не нужен, слайдер двигается только по горизонтали
+
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+
+      function onMouseMove(event) {
+        let newLeft = event.clientX - shiftX - slider.getBoundingClientRect().left;
+
+        // курсор вышел из слайдера => оставить бегунок в его границах.
+        if (newLeft < 0) {
+          newLeft = 0;
+        }
+        let rightEdge = slider.offsetWidth - thumb.offsetWidth;
+        if (newLeft > rightEdge) {
+          newLeft = rightEdge;
+        }
+
+        thumb.style.left = newLeft + 'px';
+      }
+
+      function onMouseUp() {
+        document.removeEventListener('mouseup', onMouseUp);
+        document.removeEventListener('mousemove', onMouseMove);
+      }
+
+    };
+
+    thumb.ondragstart = function() {
+      return false;
+    };
+
+    let thumb1   = slider1.querySelector('.thumb1');
+
+    thumb1.onmousedown = function(event) {
+      event.preventDefault(); // предотвратить запуск выделения (действие браузера)
+
+      let shiftX = event.clientX - thumb1.getBoundingClientRect().left;
+      // shiftY здесь не нужен, слайдер двигается только по горизонтали
+
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+
+      function onMouseMove(event) {
+        let newLeft = event.clientX - shiftX - slider1.getBoundingClientRect().left;
+
+        // курсор вышел из слайдера => оставить бегунок в его границах.
+        if (newLeft < 0) {
+          newLeft = 0;
+        }
+        let rightEdge = slider1.offsetWidth - thumb1.offsetWidth;
+        if (newLeft > rightEdge) {
+          newLeft = rightEdge;
+        }
+
+        thumb1.style.left = newLeft + 'px';
+      }
+
+      function onMouseUp() {
+        document.removeEventListener('mouseup', onMouseUp);
+        document.removeEventListener('mousemove', onMouseMove);
+      }
+
+    };
+
+    thumb1.ondragstart = function() {
+      return false;
+    };
+
+
+    
+   let thumb2 = slider2.querySelector('.thumb2');
+
+thumb2.onmousedown = function(event) {
+  event.preventDefault(); // предотвратить запуск выделения (действие браузера)
+
+  let shiftX = event.clientX - thumb2.getBoundingClientRect().left;
+  // shiftY здесь не нужен, слайдер двигается только по горизонтали
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+
+  function onMouseMove(event) {
+    let newLeft = event.clientX - shiftX - slider2.getBoundingClientRect().left;
+
+    // курсор вышел из слайдера => оставить бегунок в его границах.
+    if (newLeft < 0) {
+      newLeft = 0;
+    }
+    let rightEdge = slider2.offsetWidth - thumb2.offsetWidth;
+    if (newLeft > rightEdge) {
+      newLeft = rightEdge;
+    }
+
+    thumb2.style.left = newLeft + 'px';
+  }
+
+  function onMouseUp() {
+    document.removeEventListener('mouseup', onMouseUp);
+    document.removeEventListener('mousemove', onMouseMove);
+  }
+};
+
+thumb2.ondragstart = function() {
+  return false;
+};
+
+
+      
+ })
